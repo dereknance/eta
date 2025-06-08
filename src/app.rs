2bcc166db8d46cbb16eafac0ca7871b2431bfcfe
Derek Nance <derek@dereknance.com>
2025-06-08 13:40:19 -0500

    Application modes

--- src/app.rs
+++ src/app.rs
@@ -8,11 +8,19 @@ use ratatui::{
 #[derive(Debug)]
 pub struct App {
     /// Is the application running?
-    pub running: bool,
+    running: bool,
     /// Counter.
-    pub counter: u8,
+    counter: u8,
     /// Event handler.
-    pub events: EventHandler,
+    events: EventHandler,
+    /// Current application mode.
+    mode: Mode,
+}
+
+#[derive(Debug)]
+pub enum Mode {
+    Index,
+    Blank,
 }
 
 impl Default for App {
@@ -21,6 +29,7 @@ impl Default for App {
             running: true,
             counter: 0,
             events: EventHandler::new(),
+            mode: Mode::Index,
         }
     }
 }
@@ -34,11 +43,13 @@ impl App {
     /// Run the application's main loop.
     pub async fn run(mut self, mut terminal: DefaultTerminal) -> color_eyre::Result<()> {
         while self.running {
-            terminal.draw(|frame| frame.render_widget(&self, frame.area()))?;
+            terminal.draw(|frame|
+                frame.render_widget(&self, frame.area()))?;
             match self.events.next().await? {
                 Event::Tick => self.tick(),
                 Event::Crossterm(event) => match event {
-                    crossterm::event::Event::Key(key_event) => self.handle_key_events(key_event)?,
+                    crossterm::event::Event::Key(key_event) =>
+                        self.handle_key_events(key_event)?,
                     _ => {}
                 },
                 Event::App(app_event) => match app_event {
@@ -54,12 +65,16 @@ impl App {
     /// Handles the key events and updates the state of [`App`].
     pub fn handle_key_events(&mut self, key_event: KeyEvent) -> color_eyre::Result<()> {
         match key_event.code {
-            KeyCode::Esc | KeyCode::Char('q') => self.events.send(AppEvent::Quit),
+            // Ctrl-C is the escape hatch to close the program
             KeyCode::Char('c' | 'C') if key_event.modifiers == KeyModifiers::CONTROL => {
                 self.events.send(AppEvent::Quit)
             }
+            // Mode-dependent keypresses
+            KeyCode::Esc => self.handle_esc_key(),
+            KeyCode::Char('q') => self.handle_q_key(),
             KeyCode::Right => self.events.send(AppEvent::Increment),
             KeyCode::Left => self.events.send(AppEvent::Decrement),
+            KeyCode::Char('b') => self.handle_b_key(),
             // Other handlers you could add here.
             _ => {}
         }
@@ -70,18 +85,47 @@ impl App {
     ///
     /// The tick event is where you can update the state of your application with any logic that
     /// needs to be updated at a fixed frame rate. E.g. polling a server, updating an animation.
-    pub fn tick(&self) {}
+    fn tick(&self) {}
 
     /// Set running to false to quit the application.
-    pub fn quit(&mut self) {
+    fn quit(&mut self) {
         self.running = false;
     }
 
-    pub fn increment_counter(&mut self) {
+    fn handle_b_key(&mut self) {
+        match self.mode {
+            Mode::Index => self.mode = Mode::Blank,
+            _ => ()
+        };
+    }
+
+    fn handle_esc_key(&mut self) {
+        match self.mode {
+            Mode::Index => self.events.send(AppEvent::Quit),
+            Mode::Blank => self.mode = Mode::Index,
+        };
+    }
+
+    fn handle_q_key(&mut self) {
+        match self.mode {
+            Mode::Index => self.events.send(AppEvent::Quit),
+            Mode::Blank => self.mode = Mode::Index,
+        };
+    }
+
+    fn increment_counter(&mut self) {
         self.counter = self.counter.saturating_add(1);
     }
 
-    pub fn decrement_counter(&mut self) {
+    fn decrement_counter(&mut self) {
         self.counter = self.counter.saturating_sub(1);
+    }
+
+    pub fn counter(&self) -> u8 {
+        self.counter
+    }
+
+    pub fn mode(&self) -> &Mode {
+        &self.mode
     }
 }
--- src/ui.rs
+++ src/ui.rs
@@ -5,7 +5,7 @@ use ratatui::{
     widgets::{Block, BorderType, Paragraph, Widget},
 };
 
-use crate::app::App;
+use crate::app::{App, Mode};
 
 impl Widget for &App {
     /// Renders the user interface widgets.
@@ -15,25 +15,49 @@ impl Widget for &App {
     // - https://docs.rs/ratatui/latest/ratatui/widgets/index.html
     // - https://github.com/ratatui/ratatui/tree/master/examples
     fn render(self, area: Rect, buf: &mut Buffer) {
-        let block = Block::bordered()
-            .title("eta")
-            .title_alignment(Alignment::Center)
-            .border_type(BorderType::Rounded);
-
-        let text = format!(
-            "This is a tui template.\n\
-                Press `Esc`, `Ctrl-C` or `q` to stop running.\n\
-                Press left and right to increment and decrement the counter respectively.\n\
-                Counter: {}",
-            self.counter
-        );
-
-        let paragraph = Paragraph::new(text)
-            .block(block)
-            .fg(Color::Cyan)
-            .bg(Color::Black)
-            .centered();
-
-        paragraph.render(area, buf);
+        match self.mode() {
+            Mode::Index => render_index(self, area, buf),
+            Mode::Blank => render_blank(area, buf),
+        };
     }
+}
+
+fn render_index(app: &App, area: Rect, buf: &mut Buffer) {
+    let block = Block::bordered()
+        .title("eta")
+        .title_alignment(Alignment::Center)
+        .border_type(BorderType::Rounded);
+
+    let text = format!(
+        "This is a tui template.\n\
+            Press `Esc`, `Ctrl-C` or `q` to stop running.\n\
+            Press left and right to increment and decrement the counter respectively.\n\
+            Counter: {}",
+        app.counter()
+    );
+
+    let paragraph = Paragraph::new(text)
+        .block(block)
+        .fg(Color::Cyan)
+        .bg(Color::Black)
+        .centered();
+
+    paragraph.render(area, buf);
+}
+
+fn render_blank(area: Rect, buf: &mut Buffer) {
+    let block = Block::bordered()
+        .title("eta")
+        .title_alignment(Alignment::Center)
+        .border_type(BorderType::Rounded);
+
+    let text = "";
+
+    let paragraph = Paragraph::new(text)
+        .block(block)
+        .fg(Color::Cyan)
+        .bg(Color::Black)
+        .centered();
+
+    paragraph.render(area, buf);
 }
